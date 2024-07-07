594dbb17a114b9add80026300996cc42aa51860c
brianp1n4 <brianp1n4@gmail.com>
2024-07-06 18:02:32 -0600

    Ejercicios

--- CURSO/input.py
+++ CURSO/input.py
@@ -1,1 +1,13 @@
 nombre = input("¿Como te llamas?")
+
+nombre = input('Introduce tu nombre: ')
+apellido = input('Introduce tu apellido: ')
+edad = int(input('Introduce tu edad: '))
+correo = input('Introduce tu correo electronico: ')
+telefono = input('Introduce tu telefono: ')
+
+print('Nombre '+ nombre)
+print('Apellido: '+ apellido)
+print('Tengo '+ str(edad) + ' años')   
+print('Correo: '+ correo)
+print('Telefono: ' + telefono)
